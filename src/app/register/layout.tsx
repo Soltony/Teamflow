bcbcba0eb4aeec06fc308fed9847066f0f3965f7
Soltony <solomontony1425@gmail.com>
2025-07-02 14:07:23 +0000

    make the user registration page scrollable

--- src/app/register/layout.tsx
+++ src/app/register/layout.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-8 bg-muted/40">
+    <main className="flex min-h-screen flex-col items-center bg-muted/40 p-8 py-12">
       {children}
     </main>
   );
