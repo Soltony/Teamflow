734dd153faacea6ceeffb89497f9a9e87ddac783
Soltony <solomontony1425@gmail.com>
2025-07-08 13:24:09 +0000

    you have added two collapse buttons which is messing up everything

--- src/app/projects/[id]/layout.tsx
+++ src/app/projects/[id]/layout.tsx
@@ -1,9 +1,8 @@
-import { AppShellProvider } from "@/components/app-shell";
 
 export default function ProjectDetailLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <AppShellProvider>{children}</AppShellProvider>;
+  return <>{children}</>;
 }
