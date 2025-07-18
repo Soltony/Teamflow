2580113a606fd8a7cc0b0df29aa1b7d1ceefcdb7
Soltony <solomontony1425@gmail.com>
2025-07-18 07:03:25 +0000

    The navigation for the users, roles should be horizontal

--- src/components/settings/settings-tabs.tsx
+++ src/components/settings/settings-tabs.tsx
@@ -66,7 +66,7 @@ export function SettingsTabs({
 
     return (
          <Tabs defaultValue={defaultTab} className="w-full">
-            <TabsList className={`grid w-full grid-cols-${visibleTabs.length}`}>
+            <TabsList>
                 {canManageUsers && <TabsTrigger value="users">Users</TabsTrigger>}
                 {canManageRoles && <TabsTrigger value="roles">Roles</TabsTrigger>}
                 {canManageSettings && <TabsTrigger value="statuses">Project Statuses</TabsTrigger>}
