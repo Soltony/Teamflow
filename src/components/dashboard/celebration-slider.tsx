91732b9bd0e95fa69dfc376d80f8839cc2924631
Soltony <solomontony1425@gmail.com>
2025-07-22 07:19:09 +0000

    the arrow should be inside and remove the extra space and the horizontal

--- src/components/dashboard/celebration-slider.tsx
+++ src/components/dashboard/celebration-slider.tsx
@@ -81,8 +81,8 @@ export function CelebrationSlider({ completedProjects, teams }: { completedProje
             );
           })}
         </CarouselContent>
-        <CarouselPrevious className="absolute left-[-50px] top-1/2 -translate-y-1/2" />
-        <CarouselNext className="absolute right-[-50px] top-1/2 -translate-y-1/2" />
+        <CarouselPrevious className="absolute left-4 top-1/2 -translate-y-1/2" />
+        <CarouselNext className="absolute right-4 top-1/2 -translate-y-1/2" />
       </Carousel>
     </div>
   );
