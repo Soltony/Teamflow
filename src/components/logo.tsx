2ab488df7d10c67d5a4419b3cd59b55a30d65c8c
Soltony <aliyumer44@gmail.com>
2025-10-23 12:21:43 +0000

    the logo is null so but the fav icon is work

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 export function NibLogo({ className }: { className?: string }) {
   return (
     <Image
-      src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/NIB_International_Bank_logo.png/480px-NIB_International_Bank_logo.png"
+      src="https://upload.wikimedia.org/wikipedia/commons/3/36/NIB_International_Bank_logo.png"
       alt="NIB EPMO Logo"
       width={48}
       height={48}
