4962b6aa9224b666dc91b8f12c7552f2884b8c0c
Soltony <aliyumer44@gmail.com>
2025-10-23 07:46:48 +0000

    update the view of the task details page make it clear, attractive and u

--- src/app/tasks/[id]/page.tsx
+++ src/app/tasks/[id]/page.tsx
@@ -40,9 +40,14 @@ function LoadingSkeleton() {
     <div className="p-4 sm:p-6 space-y-6">
         <Skeleton className="h-6 w-48 mb-4" />
         <Skeleton className="h-48 w-full" />
-        <div className="grid grid-cols-2 gap-4">
-            <Skeleton className="h-64 w-full" />
-            <Skeleton className="h-64 w-full" />
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
+            <div className="lg:col-span-2 space-y-6">
+              <Skeleton className="h-64 w-full" />
+            </div>
+            <div className="space-y-6">
+              <Skeleton className="h-48 w-full" />
+              <Skeleton className="h-48 w-full" />
+            </div>
         </div>
     </div>
   )
